8d3f1314a59c31e9811aa76021d804ecf6b67c14
SamPop1234 <samuel.popoola@ust.edu.ng>
2023-09-08 12:03:23 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main- main function
+ * Return: retuns 0 if successful
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}

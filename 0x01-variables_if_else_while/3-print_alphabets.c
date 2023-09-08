38259c0c31ff925121f1a2b75cde38230fce2bd3
SamPop1234 <samuel.popoola@ust.edu.ng>
2023-09-08 11:27:15 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main- main function
+ * Return: returns 0 on success
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	for (i = 'A'; i <= 'Z'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}

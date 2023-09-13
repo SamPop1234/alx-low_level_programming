6503e19428b8bc65c14f29206cb6d3c797a2cef0
SamPop1234 <samuel.popoola@ust.edu.ng>
2023-09-13 06:52:41 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet-Function to print alphabet
+ *  Return: returns zero
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		_putchar(i);
+	_putchar('\n');
+}

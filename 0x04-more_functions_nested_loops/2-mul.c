de05784a98fac3a7a8d655f5c058e894bd1fef94
SamPop1234 <samuel.popoola@ust.edu.ng>
2023-09-14 10:06:20 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul- function that multiplies
+ * @a: parameter 1
+ * @b: parameter 2
+ * Return: multiplication result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}

44f16e1471caecbad7e7faa5627185e4ac1eca16
SamPop1234 <samuel.popoola@ust.edu.ng>
2023-09-20 22:47:45 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,33 +1,16 @@
 
 #include "main.h"
-
-
 /**
-
-  *_strcat - main function of the prototype
-
-  *
-
+  * _strcat - main function of the prototype
   * @src: Concatenation parameter
-
-  *
-
   * @dest: Concatenation parameter
-
-  *
-
   * Return: dest
-
   */
-
-
 char *_strcat(char *dest, char *src)
 {
 
 int k, r;
-
 k = 0;
-
 r = 0;
 
 while (dest[k] != '\0')
@@ -37,8 +20,8 @@ while (dest[k] != '\0')
 while (src[r] != '\0')
 {
 	dest[k] = src[r];
-        k++;
-        r++;
+	k++;
+	r++;
 }
 dest[k] = '\0';
 return (dest);

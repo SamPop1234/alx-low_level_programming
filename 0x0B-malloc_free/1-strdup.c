24b8518fb37c303fcfcfbad57f9ae33c8e4e4b18
SamPop1234 <samuel.popoola@ust.edu.ng>
2023-10-03 15:20:38 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strdup- copies string to new memory
+ * @str: string to be copied
+ * Return: pointer to new memory
+ */
+char *_strdup(char *str)
+{
+	int i;
+	char *copy;
+	int count = 0;
+
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		count++;
+	copy = malloc(sizeof(char) * count + 1);
+	if (copy == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		copy[i] = str[i];
+	return (copy);
+}
